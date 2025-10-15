06ba45ea8f5d780bed55c280e7bf793686015cbb
Mikhail Gordeev <miha_gord@mail.ru>
2025-10-15 12:59:06 +0700

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -4,18 +4,17 @@ import calendar
 def time_to_special_format(time: str) -> None:
     time_parts = re.split(r'[/ :]+', time)
     month = int(time_parts[0])
-    if month > 12:
-        print('Ошибка в вводе времени')
-        return None
-
-    year = time_parts[2]
-
     day = int(time_parts[1])
-    if day > calendar.monthrange(int(year), month)[1]:
+    year = time_parts[2]    
+    hour = int(time_parts[3])
+    minute = int(time_parts[4])
+    second = int(time_parts[5])
+
+    if (month > 12 or month < 1) or (minute > 59 or minute < 0) or (second > 59 or second < 0) \
+        or (day > calendar.monthrange(int(year), month)[1] or day < 1):
         print('Ошибка в вводе времени')
         return None
-
-    hour = int(time_parts[3])
+    
     if hour > 24:
         print('Ошибка в вводе времени')
         return None
@@ -23,21 +22,9 @@ def time_to_special_format(time: str) -> None:
         hour = f'{hour}AM'
     else:
         hour = f'{hour}PM'
-    
-    minute = int(time_parts[4])
-    if minute > 59:
-        print('Ошибка в вводе времени')
-        return None
-    
-    second = int(time_parts[5])
-    if second > 59:
-        print('Ошибка в вводе времени')
-        return None
+        
     
     time_in_special_format = f'{day}.{month}.{year[2:4]} {hour}:{minute}:{second}'
 
     print(time_in_special_format)
-
-
-
 time_to_special_format(input('Введите дату и время: '))
